78742ea6777bf8f33b9f796c6959fa23d4eacb0a
Yahya ElAraby <yaya1426@gmail.com>
2025-03-07 01:26:24 +0200

    Add Admin Role Middleware

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth', 'verified'])->group(function () {
+Route::middleware(['auth', 'verified', 'role:admin'])->group(function () {
     // Dashboard Route
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
